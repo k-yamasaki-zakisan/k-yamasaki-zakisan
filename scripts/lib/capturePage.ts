755a749f15a9878d2af6cb1fae62cd3e3016a9d2
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2023-05-05 20:43:02 +0900

    puppeteer version search

--- scripts/lib/capturePage.ts
+++ scripts/lib/capturePage.ts
@@ -1,4 +1,4 @@
-import puppeteer from "https://deno.land/x/puppeteer@14.1.1/mod.ts";
+import puppeteer from "https://deno.land/x/puppeteer@9.0.2/mod.ts";
 
 export const capturePage = async (user: string) => {
   const browser = await puppeteer.launch();
